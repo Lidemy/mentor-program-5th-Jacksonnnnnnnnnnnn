fe8f939d2668f05af8704284eac649d9b003dacd
Jacksonnnnnnnnnnnn <jackson09@live.hk>
2021-04-20 20:16:18 +0800

    week2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,10 @@
 function capitalize(str) {
-  
+  if ((/[a-zA-Z]/).test(str[0]) == true) {
+      str2 = str[0].toUpperCase() + str.slice(1)
+      return str2;
+  } else {
+      return str;
+  }
 }
 
-console.log(capitalize('hello'));
+console.log(capitalize(',hello'));
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,20 @@
 function join(arr, concatStr) {
-  
+    let arr2 = '';
+    for (let i=0; i < arr.length; i++) {
+        arr2 += arr[i]+concatStr
+    }
+    arr2 = arr2.slice(0, -1)
+    return arr2;
 }
 
 function repeat(str, times) {
-  
+    str2 = ''
+    while (times > 0) {
+        str2 += str
+        times --
+    }
+    return str2
 }
 
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+console.log(join(["a", 1, "b", 2, "c", 3], ','));
+console.log(repeat('yoyo', 2));
