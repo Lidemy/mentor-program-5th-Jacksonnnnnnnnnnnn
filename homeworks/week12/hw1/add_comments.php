cdeeb937a67e5c006936c5fd4e43f598b1688571
Jacksonnnnnnnnnnnn <jackson09@live.hk>
2021-07-18 00:45:06 +0800

    week12_hw1-3

---
+++ homeworks/week12/hw1/add_comments.php
@@ -0,0 +1,47 @@
+<?php
+  require_once('conn.php');
+  header('Content-type:application/json;charset=utf-8');
+  if (
+    empty($_POST['content']) ||
+    empty($_POST['nickname']) ||
+    empty($_POST['site_key'])
+  ) {
+    $json = array(
+      "ok" => false,
+      "message" => "Please input missing fields"
+    );
+
+    $response = json_encode($json);
+    echo $response;
+    die();
+  }
+
+  $nickname = $_POST['nickname'];
+  $site_key = $_POST['site_key'];
+  $content = $_POST['content'];
+
+  $sql = "INSERT INTO jackson_discussions(site_key, nickname, content)
+    VALUE (?,?,?)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param('sss', $site_key,$nickname,$content);
+  $result = $stmt->execute();
+
+  if(!$result) {
+    $json = array(
+      "ok" => false,
+      "message" => $conn->error
+    );
+    $response = json_encode($json);
+    echo $response;
+    die();
+  }
+
+  $json = array(
+    "ok" => true,
+    "message" => "success"
+  );
+
+  $response = json_encode($json);
+  echo $response;
+
+?>
